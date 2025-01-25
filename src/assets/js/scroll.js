4dcf99370951d5fc831faba78c31911f4ed36a19
AdilkhanBerikov <fant333777@gmail.com>
2025-01-25 16:59:10 +0500

    fixed scroll.js and bem

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -1,22 +1,39 @@
 const scrollWrapper = document.querySelector('.scroll-wrapper'); // Контейнер с прокруткой
-const scrollLeftButton = document.querySelector('.scroll-left'); // Кнопка "Назад"
-const scrollRightButton = document.querySelector('.scroll-right'); // Кнопка "Вперёд"
+const scrollButtons = document.querySelectorAll('.scroll');
+// const scrollLeftButton = document.querySelector('.scroll-left'); // Кнопка "Назад"
+// const scrollRightButton = document.querySelector('.scroll-right'); // Кнопка "Вперёд"
 
 // Задаём шаг прокрутки
-const scrollStep = 590; // Количество пикселей для прокрутки за раз
+// const scrollStep = 590; // Количество пикселей для прокрутки за раз
 
-// Обработчик для кнопки "Назад"
-scrollLeftButton.addEventListener('click', () => {
-    scrollWrapper.scrollBy({
-        left: -scrollStep, // Прокрутка влево
-        behavior: 'smooth', // Плавная прокрутка
+scrollButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('-left')) {
+            scrollWrapper.scrollBy({
+                left: -590, // Прокрутка влево
+                behavior: 'smooth',
+            });
+        } else if (button.classList.contains('-right')) {
+            scrollWrapper.scrollBy({
+                left: 590, // Прокрутка вправо
+                behavior: 'smooth',
+            });
+        }
     });
 });
 
-// Обработчик для кнопки "Вперёд"
-scrollRightButton.addEventListener('click', () => {
-    scrollWrapper.scrollBy({
-        left: scrollStep, // Прокрутка вправо
-        behavior: 'smooth', // Плавная прокрутка
-    });
-});
+// // Обработчик для кнопки "Назад"
+// scrollLeftButton.addEventListener('click', () => {
+//     scrollWrapper.scrollBy({
+//         left: -scrollStep, // Прокрутка влево
+//         behavior: 'smooth', // Плавная прокрутка
+//     });
+// });
+//
+// // Обработчик для кнопки "Вперёд"
+// scrollRightButton.addEventListener('click', () => {
+//     scrollWrapper.scrollBy({
+//         left: scrollStep, // Прокрутка вправо
+//         behavior: 'smooth', // Плавная прокрутка
+//     });
+// });
